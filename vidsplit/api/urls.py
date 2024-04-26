79663d771ffeeffdd518cf9c65fb3ac090248d30
Mattic <50807277+ImMattic@users.noreply.github.com>
2024-04-25 23:12:29 -0500

    api changes - end of #5

--- vidsplit/api/urls.py
+++ vidsplit/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import main
+from .views import RoomView, CreateRoomView
 
 urlpatterns = [
-    path('', main),
+    path("room", RoomView.as_view()),
+    path("create-room", CreateRoomView.as_view()),
 ]
--- vidsplit/api/views.py
+++ vidsplit/api/views.py
@@ -1,8 +1,48 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics, status
+from .serializers import RoomSerializer, CreateRoomSerializer
+from .models import Room
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 
 
-def main(request):
-    return HttpResponse("Hello")
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+
+class CreateRoomView(APIView):
+    serializer_class = CreateRoomSerializer
+
+    def post(self, request, format=None):
+        # If session does not exist, create one
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            guest_can_pause = serializer.data.get("guest_can_pause")
+            votes_to_skip = serializer.data.get("votes_to_skip")
+            host = self.request.session.session_key
+            # Check if a room already exists for the host
+            queryset = Room.objects.filter(host=host)
+            # If it does, then update the room with the new settings
+            if queryset.exists():
+                room = queryset[0]
+                room.guest_can_pause = guest_can_pause
+                room.votes_to_skip = votes_to_skip
+                room.save(update_fields=["guest_can_pause", "votes_to_skip"])
+                return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
+            # If it does not, then create a new room
+            else:
+                room = Room(
+                    host=host,
+                    guest_can_pause=guest_can_pause,
+                    votes_to_skip=votes_to_skip,
+                )
+                room.save()
+
+            # Returns JSON data to the client
+        return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
